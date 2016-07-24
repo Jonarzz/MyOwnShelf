42db286db2329c600a16cb8598171570222cb49d
varsheim <lzielinski66@gmail.com>
2016-07-24 11:09:14 +0200

    Zbudowana aplikacja main_resources
    Tymczasowy plik z bazą danych *.sqlite3 dodany do .gitignore

--- myownshelf/myownshelf/settings.py
+++ myownshelf/myownshelf/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_resource.apps.MainResourceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
